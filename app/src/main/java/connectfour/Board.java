8b7eddfb1b83ddc7f2ff75bced8fd03d4f658c02
Patrick Bucher <patrick.bucher@edulu.ch>
2021-06-27 13:00:19 +0200

    initial commit

---
+++ app/src/main/java/connectfour/Board.java
@@ -0,0 +1,29 @@
+public class Board {
+    public static final char EMPTY_FIELD = ' ';
+    public static final char PLAYER_ONE_FIELD = 'x';
+    public static final char PLAYER_TWO_FIELD = 'o';
+
+    private final int rows;
+    private final int cols;
+    private final char[][] fields;
+
+    public static newDefaultBoard() {
+        return new Board(6, 7);
+    }
+
+    private Board(int rows, int cols) {
+        if (rows < 4 || cols < 4) {
+            throw new IllegalArgumentException("A board needs at least four rows and columns.");
+        }
+        this.rows = rows;
+        this.cols = cols;
+        this.fields = new char[rows];
+        for (int r = 0; r < this.rows; r++) {
+            this.fields[r] = new chars[cols];
+            for (int c = 0; c < this.cols; c++) {
+                this.fields[r][c] = EMPTY_FIELD;
+            }
+        }
+    }
+
+}
